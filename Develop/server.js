304934a8f871949cf398c41f6e3d4fa4c072be6a
bdonys <brandonsur@yahoo.com>
2024-01-08 12:13:54 -0800

    server code

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,8 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
