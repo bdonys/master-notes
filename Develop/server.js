8d0a3f6ccfc2d833d3f44909f40fc966c356e695
bdonys <brandonsur@yahoo.com>
2024-01-08 20:08:33 -0800

    cleared lines

--- Develop/server.js
+++ Develop/server.js
@@ -14,15 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-// app.get('/notes', (req,  res) => {
-//     res.sendFile(__dirname, './public/notes.html');
-// });
-
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname, './public/index.html');
-// });
-
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
